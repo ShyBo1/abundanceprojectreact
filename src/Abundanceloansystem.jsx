8ecc1bc51d2306c9dc1963305a9147a19a37092e
Shy Bo1 <topstarabundance@gmail.com>
2025-08-04 20:31:03 +0100

    Initial commit

---
+++ src/Abundanceloansystem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Abundanceloansystem() {
+  return (
+    <div>Abundanceloansystem</div>
+  )
+}
+
+export default Abundanceloansystem
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Abundanceloansystem from './Abundanceloansystem'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Abundanceloansystem />
+
+  </StrictMode>,
+)
